4ebb0e9f52301d6da99ddd88f86510b52a98f69d
italosantana <italosanttana@hotmail.com>
2021-07-30 16:23:42 -0300

    hello-world-graphql

---
+++ index.js
@@ -0,0 +1,23 @@
+const { gql, ApolloServer } = require("apollo-server")
+
+const resolvers = {
+    Query: {
+        hello(){
+            return 'World';
+        }
+    }
+
+}
+const typeDefs = gql `
+    type Query {
+        hello: String
+    }
+`
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen()
